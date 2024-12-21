a1bf832358a53d9cc7298580ccb288addfb6db30
Fabien D. <fabien@myprod.net>
2024-12-21 17:48:10 +0100

    Updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,12 +21,13 @@ function Home(): React.ReactElement {
           href="https://cloud.google.com/sdk/gcloud/reference/beta/emulators"
           target="_blank"
           className="underline decoration-solid underline-offset-4 decoration-pink-500"
+          rel="noreferrer"
         >
           Google Cloud Platform™ Emulators
         </a>
         .
         <br />
-        It's{' '}
+        It&apos;s{' '}
         <span className="underline decoration-solid underline-offset-4 decoration-pink-500">
           not an official
         </span>{' '}
@@ -35,12 +36,12 @@ function Home(): React.ReactElement {
       <Typography variant="body1" className="mt-4">
         I started the project as a simple test, to play with Tauri-App V2,
         ReactJs & Typescript and finally implement something that works with
-        Pub/Sub. The project is under development and doesn't cover all the
-        emulator APIs and isn't covered by tests. Despite this, I found the
+        Pub/Sub. The project is under development and doesn&apos;t cover all the
+        emulator APIs and isn&apos;t covered by tests. Despite this, I found the
         project useful and decided to keep it and develop it further.
       </Typography>
 
-      <Typography variant="h2">Let's play</Typography>
+      <Typography variant="h2">Let&apos;s play</Typography>
       <EmulatorSettings
         host={emulator ? emulator.host : 'localhost'}
         port={emulator ? emulator.port : 8085}
@@ -94,6 +95,7 @@ function Home(): React.ReactElement {
           href="https://github.com/FabienD/gui-for-gcp-emulator"
           target="_blank"
           className="underline decoration-solid underline-offset-4 decoration-pink-500"
+          rel="noreferrer"
         >
           feel free to contribute and/or use it
         </a>
