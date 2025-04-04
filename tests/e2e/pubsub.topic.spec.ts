36b8ab50af9f8b97f589edf2f868807bd55f3d1b
Fabien D. <fabien@myprod.net>
2025-04-04 21:26:49 +0200

    Add new e2e tests (wip)

--- tests/e2e/pubsub.topic.spec.ts
+++ tests/e2e/pubsub.topic.spec.ts
@@ -1,28 +1,34 @@
 import { test, expect } from './pubsub.fixture';
 
 test.describe('PubSub Topic', () => {
-
   test('I should see the PubSub Topic page', async ({ page }) => {
     await page.goto('/pubsub');
     const heading = page.locator('main h1');
     await expect(heading).toHaveText('Pub/Sub');
   });
-  
-  test('I should see an alert if the emulator is not connected', async ({ page }) => {
+
+  test('I should see an alert if the emulator is not connected', async ({
+    page,
+  }) => {
     await page.goto('/pubsub');
     const alert = page.getByRole('alert');
     const alertText = alert.locator('.MuiAlert-message');
-    
-    await expect(alertText).toHaveText('The emulator is not configured or the connection is not validated.');
+
+    await expect(alertText).toHaveText(
+      'The emulator is not configured or the connection is not validated.',
+    );
   });
 
-  test('I should see an empty list of topic if the emulator is connected', async ({ page, pubsubConnection }) => {
+  test('I can create a topic, then see it in the list', async ({
+    page,
+    pubsubConnection,
+  }) => {
     await pubsubConnection.connect('/', 'localhost', '8085', 'test-project');
     await page.locator('#PubSub a').click();
-    
-    const alertText = page.getByRole('alert').locator('.MuiAlert-message');
-    
-    await expect(alertText).toHaveText('No topics');
+
+    const topicName = 'topic-1';
+    await page.locator('form #name').fill(topicName);
+
+    await page.getByRole('button', { name: 'Create', exact: true }).click();
   });
 });
-
