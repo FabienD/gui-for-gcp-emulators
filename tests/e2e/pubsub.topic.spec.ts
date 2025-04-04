e340ecc2235fea55d9816bdea5b0aa8908f99332
Fabien Dosse <fabien.dosse@akeneo.com>
2025-04-04 09:11:57 +0200

    Add test fixtures

--- tests/e2e/pubsub.topic.spec.ts
+++ tests/e2e/pubsub.topic.spec.ts
@@ -1,33 +1,28 @@
-import { test, expect } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/pubsub');
-});
-
+import { test, expect } from './pubsub.fixture';
 
 test.describe('PubSub Topic', () => {
 
   test('I should see the PubSub Topic page', async ({ page }) => {
-    
-    const heading = await page.locator('main h1');
+    await page.goto('/pubsub');
+    const heading = page.locator('main h1');
     await expect(heading).toHaveText('Pub/Sub');
   });
   
   test('I should see an alert if the emulator is not connected', async ({ page }) => {
-    
-    const alert = await page.getByRole('alert');
-    const alertText = await alert.locator('.MuiAlert-message');
+    await page.goto('/pubsub');
+    const alert = page.getByRole('alert');
+    const alertText = alert.locator('.MuiAlert-message');
     
     await expect(alertText).toHaveText('The emulator is not configured or the connection is not validated.');
   });
 
-  test('I should see an empty list of topic if the emulator is connected', async ({ page }) => {
+  test('I should see an empty list of topic if the emulator is connected', async ({ page, pubsubConnection }) => {
+    await pubsubConnection.connect('/', 'localhost', '8085', 'test-project');
+    await page.locator('#PubSub a').click();
     
-
-    const alert = await page.getByRole('alert');
-    const alertText = await alert.locator('.MuiAlert-message');
+    const alertText = page.getByRole('alert').locator('.MuiAlert-message');
     
-    await expect(alertText).toHaveText('No Topics');
+    await expect(alertText).toHaveText('No topics');
   });
 });
 
